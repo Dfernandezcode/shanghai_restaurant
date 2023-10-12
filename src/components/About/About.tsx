e4924956924c10d2ed2e1acc5de1128a34c43e1b
Daniel Fernandez <dfernandezcode@gmail.com>
2023-10-13 00:09:38 +0200

    added about

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -5,7 +5,8 @@ const About = () => {
   return (
     <div className="About">
       <Nav currentLocale={""} onLocaleChange={(_locale: string): void => {}} />
-      <h1>Our About</h1>
+      <h1>Our History</h1>
+      <p>Established in 1971, "Shanghai" stands as Balikpapan's oldest traditional Chinese restaurant, offering a rich culinary journey into authentic flavors of the Far East. For over five decades, we have been a haven for food enthusiasts, delivering meticulously crafted dishes using time-honored recipes and the freshest ingredients. Our elegant and nostalgic ambiance provides a gateway to the vibrant, aromatic world of Chinese cuisine. At "Shanghai," every meal is not just a dining experience but a celebration of Chinese culture and gastronomy, right in the heart of Balikpapan.</p>
     </div>
   );
 };
